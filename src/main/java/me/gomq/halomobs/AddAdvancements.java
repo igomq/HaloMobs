98fcec69e5ae59ea88ed3a7e19076e07c5293396
Gom0927 <gom@gomq.me>
2022-10-30 19:25:12 +0900

    Fixed Bug (Plugin Not Loading)

--- src/main/java/me/gomq/halomobs/AddAdvancements.java
+++ src/main/java/me/gomq/halomobs/AddAdvancements.java
@@ -45,32 +45,32 @@ public class AddAdvancements {
         );
 
         Advancements.CraftAdv createEgg = new Advancements.CraftAdv("create_egg_item",
-                new AdvancementDisplay(Material.TURTLE_EGG, "Egg Enterance", AdvancementFrameType.TASK, true, true, 1.5f, 1.5f, "Craft an Egg Item"),
+                new AdvancementDisplay(Material.TURTLE_EGG, "Egg Enterance", AdvancementFrameType.TASK, true, true, 1.5f, 4.5f, "Craft an Egg Item"),
                 Egg, Egg.asQuantity(16),
                 rootAdvancement);
 
         Advancements.UseAdv useEgg = new Advancements.UseAdv("use_egg_item",
-                new AdvancementDisplay(Material.TURTLE_EGG, "Egg Master", AdvancementFrameType.TASK, true, true, 3f, 1.5f, "Use an Egg Item thirty-two times"),
+                new AdvancementDisplay(Material.TURTLE_EGG, "Egg Master", AdvancementFrameType.TASK, true, true, 3f, 4.5f, "Use an Egg Item thirty-two times"),
                 Egg, new ItemStack(Material.END_CRYSTAL, 4),
                 createEgg, 32);
 
         Advancements.KillAdv killGolem = new Advancements.KillAdv("kill_golem",
-                new AdvancementDisplay(Material.IRON_BLOCK, "Golem Killer", AdvancementFrameType.TASK, true, true, 1.5f, -1.5f, "Kill Iron Golem twice"),
+                new AdvancementDisplay(Material.IRON_BLOCK, "Golem Killer", AdvancementFrameType.TASK, true, true, 1.5f, 1.5f, "Kill Iron Golem twice"),
                 EntityType.IRON_GOLEM, new ItemStack(Material.IRON_BLOCK, 16),
                 rootAdvancement, 2);
 
         Advancements.KillAdv killBlaze = new Advancements.KillAdv("kill_blaze",
-                new AdvancementDisplay(Material.BLAZE_ROD, "Blaze Killer", AdvancementFrameType.TASK, true, true, 3f, -1.5f, "Kill Blaze ten times"),
+                new AdvancementDisplay(Material.BLAZE_ROD, "Blaze Killer", AdvancementFrameType.TASK, true, true, 3f, 1.5f, "Kill Blaze ten times"),
                 EntityType.BLAZE, new ItemStack(Material.BLAZE_ROD, 16),
                 killGolem, 10);
 
         Advancements.KillAdv killEnderman = new Advancements.KillAdv("kill_enderman",
-                new AdvancementDisplay(Material.ENDER_PEARL, "End Slayer", AdvancementFrameType.TASK, true, true, 4.5f, -1.5f, "Kill Enderman twenty times"),
+                new AdvancementDisplay(Material.ENDER_PEARL, "End Slayer", AdvancementFrameType.TASK, true, true, 4.5f, 1.5f, "Kill Enderman twenty times"),
                 EntityType.ENDERMAN, new ItemStack(Material.ELYTRA, 1),
                 killBlaze, 20);
 
         Advancements.KillAdv killWitherSkell = new Advancements.KillAdv("kill_wither_skell",
-                new AdvancementDisplay(Material.WITHER_SKELETON_SKULL, "Wither Skell Killer", AdvancementFrameType.TASK, true, true, 4.5f, -3f, "Kill Wither Skeleton five times"),
+                new AdvancementDisplay(Material.WITHER_SKELETON_SKULL, "Wither Skell Killer", AdvancementFrameType.TASK, true, true, 4.5f, 3f, "Kill Wither Skeleton five times"),
                 EntityType.WITHER_SKELETON, new ItemStack(Material.WITHER_SKELETON_SKULL, 3),
                 killBlaze, 5);
 
